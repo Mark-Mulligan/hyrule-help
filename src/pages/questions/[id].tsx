2f8bf2beb25905c194842a7938141aabe5373bdb
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2023-05-31 19:10:11 -0500

    working on question page style

--- src/pages/questions/[id].tsx
+++ src/pages/questions/[id].tsx
@@ -16,11 +16,40 @@ const QuestionPage = () => {
   console.log(questionQuery.data);
 
   return (
-    <main className="container mx-auto max-w-4xl bg-base-100 pb-16 pt-24">
-      <h1 className="text-center text-5xl font-bold">
-        {questionQuery.data?.title}
-      </h1>
-      <p>{questionQuery.data?.content}</p>
+    <main className="container mx-auto min-h-screen max-w-4xl bg-base-100 bg-opacity-80 px-4 pb-16 pt-24">
+      <div className="mx-auto max-w-2xl">
+        <h1 className="mb-4 text-center text-5xl font-bold">
+          {questionQuery.data?.title}
+        </h1>
+        <ul>
+          <div className="grid grid-cols-2">
+            <li className="mb-2">
+              <h2 className="text-lg font-bold">Categories</h2>
+              <ul>
+                {questionQuery.data?.categories.split(",").map((category) => {
+                  return (
+                    <li className="badge-outline badge mr-2" key={category}>
+                      {category}
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+            <li className="mb-2">
+              <h2 className="text-lg font-bold">Game</h2>
+              <p>
+                {questionQuery.data?.game === "botw"
+                  ? "Breath of the Wild"
+                  : "Tears of the Kingdom"}
+              </p>
+            </li>
+          </div>
+          <li className="mb-2">
+            <h2 className="text-lg font-bold">Question</h2>
+            <p>{questionQuery.data?.content}</p>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 };
