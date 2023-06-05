01bd0ff53f1535825519561e2484b86397fb78de
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2023-06-05 16:03:05 -0500

    added ability to add test users

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,52 @@
+// react
+import { useState } from "react";
+
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <main className="container mx-auto max-w-lg pt-20">
+      <div className="card bg-base-100 shadow-xl">
+        <form className="card-body">
+          <h2 className="card-title">Sign up</h2>
+
+          <ul className="mb-4">
+            <li className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Username</span>
+              </label>
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                placeholder="username"
+                className="input-bordered input w-full"
+                required
+              />
+            </li>
+            <li className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="text"
+                placeholder="password"
+                className="input-bordered input w-full"
+                required
+              />
+            </li>
+          </ul>
+
+          <div className="card-actions justify-end">
+            <button className="btn-primary btn">Create Account</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default SignUp;
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,4 +1,5 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { type GetServerSidePropsContext } from "next";
 import {
   getServerSession,
@@ -53,16 +54,38 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        console.log({ credentials, req });
+        const username = credentials?.username;
+        const password = credentials?.password;
 
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+        const user = await prisma.user.findFirst({ where: { name: username } });
+
+        if (user && password) {
+          if (user.password === password) {
+            return user;
+          }
+
+          return null;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
   ],
   session: {
     strategy: "jwt",
