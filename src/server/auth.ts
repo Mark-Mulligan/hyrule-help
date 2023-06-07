04e26f85e4b738b0d584cb6440c4cc557d6f5252
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2023-06-07 15:56:43 -0500

    improved login page

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,6 +48,9 @@ export const authOptions: NextAuthOptions = {
       };
     },
   },
+  pages: {
+    signIn: "/auth/sign-in",
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
